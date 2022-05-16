757740dc5feccfa1c08685210a023bb5431976c9
DaiKfD <nguyentrongdai14042000@gmail.com>
2022-05-16 23:45:27 +0700

    AddLimit

---
+++ 04.my-microservices/spring-cloud-config-sever/src/main/java/com/in28minutes/microservices/springcloudconfigsever/SpringCloudConfigSeverApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.springcloudconfigsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigSeverApplication.class, args);
+	}
+
+}
